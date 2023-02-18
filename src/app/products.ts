97004e26cc6047656e00f3968cc85ede71cf15b9
oidai <oikawa.daiki@us-factory.jp>
2023-02-18 16:33:09 +0900

    画面構成完成

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,16 +2,36 @@ export const products = [
   {
     name:'Phone XL',
     price: 799,
-    description:'~~~~~~'
+    description:'~~~~~~',
+    hedding1:'AAA',
+    hedding2:'',
+    hedding3:'',
+    coverimage:'./assets/img/phone-cover.jpg'
   },
   {
     name:'Phone mini',
     price: 699,
-    description:'~~~~~~'
+    description:'~~~~~~',
+    hedding1:'',
+    hedding2:'',
+    hedding3:'',
+    coverimage:'./assets/img/phone-cover.jpg'
   },
   {
     name:'Phone Standard',
     price: 299,
-    description:'~~~~~~'
+    description:'~~~~~~',
+    hedding1:'',
+    hedding2:'',
+    hedding3:''
+  },
+  {
+    name:'Phone spcial',
+    price: 1299,
+    description:'~~~~~~',
+    hedding1:'',
+    hedding2:'',
+    hedding3:''
   }
+  
 ]
